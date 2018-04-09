64f85deace3519d8665017ed09650b5cd093affc
reububble <reububble@gmail.com>
2018-04-09 14:39:19 +1000

    Center style
    using table display

--- web/resources/js/permanent.js
+++ web/resources/js/permanent.js
@@ -1,7 +1,9 @@
-var a = document.getElementsByTagName("a");
-for (var i = 0; i < a.length; i++) {
-    a[i].onclick = function() {
-        window.location = this.getAttribute("href");
-        return false;
-    }
+window.onload = function() {
+	var a = document.getElementsByTagName("a");
+	for (var i = 0; i < a.length; i++) {
+		a[i].onclick = function() {
+			window.location = this.getAttribute("href");
+			return false;
+	    }
+	}
 }
